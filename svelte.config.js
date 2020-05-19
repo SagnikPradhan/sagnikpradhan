20710f215bfb690ed3731e4a2e10b411ea18e269
Sagnik Pradhan <markten6321@gmail.com>
2020-05-19 11:59:13 +0530

    Reset

--- svelte.config.js
+++
@@ -1,9 +0,0 @@
-import autoPreprocess from 'svelte-preprocess'
-
-module.exports = {
-    dev: true,
-    css: (css) => {
-        css.write('public/build/bundle.css')
-    },
-    preprocess: autoPreprocess(),
-}
